4fa66f0789a48c913c5c594671d0c28bcdc8013f
Josueeeee <jeffreycap12@gmail.com>
2024-07-01 10:35:01 -0600

    update variables de entorno

--- src/firbaseFuntion.js
+++ src/firbaseFuntion.js
@@ -1,18 +1,19 @@
-const functions = require("firebase-functions");
-const admin = require("firebase-admin");
-admin.initializeApp();
-const db = admin.firestore();
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-exports.updateRequestStatus = functions.https.onCall(async (data, context) => {
-  const { requestId, status } = data;
+// Configuración de Firebase usando variables de entorno
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
-  try {
-    const requestRef = db.collection("formSubmissions").doc(requestId);
-    await requestRef.update({ status });
-
-    return { success: true, message: `Request ${status}` };
-  } catch (error) {
-    console.error("Error updating request status:", error);
-    return { success: false, message: "Failed to update request status" };
-  }
-});
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
